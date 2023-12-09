5474872620b60c74f12f5bc9cc03bb659646319c
RuiQi Tan <RuiQi2010@outlook.com>
2023-12-09 21:35:29 +0800

    Update #2

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let score = document.querySelector('#score');
 let clickpower = document.querySelector('#clickpower');
 
-var identifiers = ["pen", "ming", "liang", "lihua"];
 var mprs = [1.1, 1.2, 1.3, 1.4];
 var powers = [0, 5, 100, 2000];
 
@@ -17,23 +16,31 @@ function hval(element) {
     return parseInt(element.innerHTML);
 }
 
-function buyGeneric(identifier, mpr, handler=function() {}) {
-    var cost = hget(identifier + "-cost");
+function hset(element, value) {
+    element.innerHTML = value;
+}
+
+function hadd(element, value) {
+    element.innerHTML = hval(element) + value;
+}
+
+function buyGeneric(i, mpr, handler=function() {}) {
+    var cost = hget(i + "c");
     if (hval(score) >= hval(cost)) {
-        score.innerHTML = hval(score) - hval(cost);
-        cost.innerHTML = Math.ceil(hval(cost) * mpr);
+        hadd(score, -hval(cost));
+        hset(cost, Math.ceil(hval(cost) * mpr));
         handler();
-        hget(identifier + "s").innerHTML = hval(hget(identifier + "s")) + 1;
-        if (hval(score) < hval(cost)) hget(identifier).classList.remove("upgradable");
+        hadd(hget(i + "s"), 1);
+        if (hval(score) < hval(cost)) hget(i).classList.remove("upgradable");
     }
 }
 
 function buy(index, handler=function() {}) {
-    buyGeneric(identifiers[index], mprs[index], handler);
+    buyGeneric(index.toString(), mprs[index], handler);
 }
 
-function checkUpgradable(identifier) {
-    if (hval(score) >= hval(hget(identifier + "-cost"))) hget(identifier).classList.add("upgradable");
+function checkUpgradable(i) {
+    if (hval(score) >= hval(hget(i.toString() + "c"))) hget(i.toString()).classList.add("upgradable");
 }
 
 function increment(num=0) {
@@ -42,8 +49,8 @@ function increment(num=0) {
 }
 
 setInterval(function() {
-    for (let i = 0; i < identifiers.length; i++) {
-        checkUpgradable(identifiers[i]);
-        if (hval(hget(identifiers[i] + "s")) && i) increment(powers[i] * hval(hget(identifiers[i] + "s")));
+    for (let i = 0; i < mprs.length; i++) {
+        checkUpgradable(i);
+        if (hval(hget(i.toString() + "s")) && i) increment(powers[i] * hval(hget(i.toString() + "s")));
     }
 }, 50); 
