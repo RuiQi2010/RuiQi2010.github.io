0b727b23f36fcdf2a5b4c76d2d03553412470924
RuiQi Tan <RuiQi2010@outlook.com>
2023-12-14 22:13:34 +0800

    Update #3

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let score = document.querySelector('#score');
 let clickpower = document.querySelector('#clickpower');
+var currentActiveElement = document.getElementById('base');
+let cotton = document.querySelector('#cs');
 
 var mprs = [1.1, 1.2, 1.3, 1.4];
 var powers = [0, 5, 100, 2000];
+var cottonlimit = 330000;
 
 function incrementClickPower() {
     clickpower.innerHTML = hval(clickpower) + 1;
@@ -10,6 +13,12 @@ function incrementClickPower() {
 
 function hget(id) {
     return document.getElementById(id);
+}
+
+function switchThing(inactiveid) {
+    currentActiveElement.classList.add('inactive');
+    currentActiveElement = document.getElementById(inactiveid);
+    document.getElementById(inactiveid).classList.remove('inactive');
 }
 
 function hval(element) {
@@ -31,7 +40,6 @@ function buyGeneric(i, mpr, handler=function() {}) {
         hset(cost, Math.ceil(hval(cost) * mpr));
         handler();
         hadd(hget(i + "s"), 1);
-        if (hval(score) < hval(cost)) hget(i).classList.remove("upgradable");
     }
 }
 
@@ -39,8 +47,17 @@ function buy(index, handler=function() {}) {
     buyGeneric(index.toString(), mprs[index], handler);
 }
 
+function buyCotton() {
+    buyGeneric("c", 1.583);
+}
+
+function checkUpgradableRaw(i) {
+    if (hval(score) >= hval(hget(i + "c"))) hget(i).classList.add("upgradable");
+    else if (hget(i).classList.contains("upgradable")) hget(i).classList.remove("upgradable");
+}
+
 function checkUpgradable(i) {
-    if (hval(score) >= hval(hget(i.toString() + "c"))) hget(i.toString()).classList.add("upgradable");
+    checkUpgradableRaw(i.toString());
 }
 
 function increment(num=0) {
@@ -49,6 +66,12 @@ function increment(num=0) {
 }
 
 setInterval(function() {
+    if (hval(score) >= cottonlimit) {
+        document.getElementById("menu-cotton").classList.remove('inactive');
+    } if (hval(score) >= 10) {
+        document.getElementById("menu-base").classList.remove('inactive');
+    }
+    checkUpgradableRaw("c");
     for (let i = 0; i < mprs.length; i++) {
         checkUpgradable(i);
         if (hval(hget(i.toString() + "s")) && i) increment(powers[i] * hval(hget(i.toString() + "s")));
